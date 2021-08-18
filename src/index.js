783b4a6f0970fd1eaa064458055888f456f7d006
marwa <marwajawad19@gmail>
2021-08-18 08:36:45 -0400

    adding redux store

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+ReactDOM.render(
+<Provider store={store}>
+<App /></Provider>, document.getElementById("root"));
